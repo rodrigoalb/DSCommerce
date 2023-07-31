618c585139ab57c4d70af80b7b4b41109cfa7274
rodrigoalb <rodrigoalba-@outlook.com>
2023-07-31 20:35:01 -0300

    Equals e HashCode

--- src/main/java/com/devsuperior/dscommerce/models/OrderItemPK.java
+++ src/main/java/com/devsuperior/dscommerce/models/OrderItemPK.java
@@ -2,6 +2,8 @@ package com.devsuperior.dscommerce.models;
 
 import jakarta.persistence.*;
 
+import java.util.Objects;
+
 @Embeddable
 public class OrderItemPK {
 
@@ -35,5 +37,18 @@ public class OrderItemPK {
 
     public void setProduct(Product product) {
         this.product = product;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderItemPK that = (OrderItemPK) o;
+        return Objects.equals(order, that.order) && Objects.equals(product, that.product);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(order, product);
     }
 }
